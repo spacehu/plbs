a62a12367bcb25c1a9296fd4d944abf1899474a7
spacehu <spacehu@tigerhuclub.com>
2019-05-14 00:52:00 -0700

    update

--- TigerDAL/Api/CourseDAL.php
+++ TigerDAL/Api/CourseDAL.php
@@ -28,10 +28,10 @@ class CourseDAL {
                 $where .= " and (c.enterprise_id=0 or c.id in (" . $ids . ") ) ";
             }
         }
-        $sql = "select c.*,i.original_src,uc.status as ucStatus "
+        $sql = "select c.*,i.original_src,if(uc.status,uc.status,0) as ucStatus "
                 . "from " . $base->table_name("course") . " as c "
                 . "left join " . $base->table_name("image") . " as i on i.id=c.media_id "
-                . "left join " . $base->table_name("user_course") . " as uc on uc.course_id=c.id and uc.user_id=" . $user_id . " "
+                . "left join " . $base->table_name("user_course") . " as uc on uc.course_id=c.id and uc.user_id=" . $user_id . " and uc.delete=0 "
                 . "where c.`delete`=0 " . $where . " "
                 . "order by c.edit_time desc "
                 . "limit " . $limit_start . "," . $limit_end . " ;";
