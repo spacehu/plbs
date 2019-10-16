e758586356c1eddbf0b46d1e2ef89175fd0512f4
spacehu <spacehu@tigerhuclub.com>
2019-10-15 20:08:20 -0700

    update for 20

--- TigerDAL/Api/EnterpriseDAL.php
+++ TigerDAL/Api/EnterpriseDAL.php
@@ -41,26 +41,58 @@ class EnterpriseDAL {
         $base = new BaseDAL();
         $limit_start = ($currentPage - 1) * $pagesize;
         $limit_end = $pagesize;
-        $sql = "SELECT "
-                . "u.id, "
-                . "u.`NAME`, "
-                . "u.photo, "
-                . "count(DISTINCT(uc.id)) AS joinCourseCount, "
-                . "count(DISTINCT(e.course_id)) AS passExamCount, "
-                . "count(ul.id) AS userLessonTotal, "
-                . "count(l.id) AS lessonCount, "
-                . "if(count(l.id)<>0,count(ul.id)/count(l.id)*100,0) as progress "
-                . "FROM " . $base->table_name("user_info") . " AS u  "
-                . "LEFT JOIN " . $base->table_name("enterprise_user") . " AS eu ON u.id = eu.user_id AND eu.enterprise_id ='" . $id . "' "
-                . "LEFT JOIN " . $base->table_name("user_course") . " AS uc ON uc.user_id = eu.user_id and uc.`delete`=0 "
-                . "LEFT JOIN " . $base->table_name("enterprise_course") . " AS ec ON ec.enterprise_id = eu.enterprise_id and uc.course_id=ec.course_id "
-                . "LEFT JOIN " . $base->table_name("exam") . " AS e ON e.course_id = uc.course_id and e.point>60 "
-                . "LEFT JOIN " . $base->table_name("lesson") . " AS l ON l.course_id = uc.course_id  "
-                . "LEFT JOIN " . $base->table_name("user_lesson") . " AS ul ON l.id = ul.lesson_id "
-                . "WHERE eu.`delete` = 0 "
-                . "AND eu.`STATUS` = 1 "
-                . "group by u.id "
-                . "limit " . $limit_start . "," . $limit_end . " ;";
+        $sql="SELECT 
+                euce.id,
+                euce.uname,
+                euce.photo,
+                COUNT(DISTINCT (euce.course_id)) AS joinCourseCount,
+                COUNT(DISTINCT (euce.eid)) AS passExamCount,
+                COUNT( (euce.ulid)) AS userLessonTotal,
+                COUNT( (euce.lid)) AS lessonCount,
+                IF(COUNT(euce.lid) <> 0,
+                    COUNT(euce.ulid) / COUNT(euce.lid) * 100,
+                    0) AS progress
+                    from 
+            (
+            SELECT 
+                u.id,
+                u.`NAME` as uname,
+                u.photo,
+                uc.course_id,
+                eu.department_id as eudid,
+                eu.position_id as eupid,
+                ec.department_id as ecdid,
+                ec.position_id as ecpid,
+                ed.`delete` as edd,
+                ep.`delete` as epd,
+                e.id as eid,e.`point` as epoint,
+                c.percentage,
+                l.id as lid,
+                ul.id as ulid
+               
+            FROM
+            " . $base->table_name("user_info") . " AS u    
+                LEFT JOIN " . $base->table_name("enterprise_user") . " AS eu ON u.id = eu.user_id
+                    LEFT JOIN " . $base->table_name("user_course") . " AS uc ON uc.user_id = eu.user_id AND uc.`delete` = 0 
+                    left join " . $base->table_name("course") . " as c on uc.course_id=c.id and c.`delete` =0 
+                    left join " . $base->table_name("enterprise_course") . " as ec on ec.course_id=uc.course_id and ec.enterprise_id=eu.enterprise_id and ec.`delete`=0 
+                    LEFT JOIN " . $base->table_name("enterprise_department") . " AS ed ON ed.id = eu.department_id 
+                    LEFT JOIN " . $base->table_name("enterprise_position") . " AS ep ON ep.id = eu.position_id 
+                    
+                    LEFT JOIN " . $base->table_name("exam") . " AS e ON e.course_id = uc.course_id and e.user_id = u.id AND e.`point` > c.percentage
+                    LEFT JOIN " . $base->table_name("lesson") . " AS l ON l.course_id = uc.course_id and l.`delete`=0
+                    LEFT JOIN " . $base->table_name("user_lesson") . " AS ul ON l.id = ul.lesson_id and ul.`delete`=0 and ul.user_id=u.id
+            WHERE
+                eu.`delete` = 0 AND eu.`STATUS` = 1
+                    AND eu.enterprise_id = '2'
+                    and (ec.department_id is null  or (ec.department_id is not null and ed.delete = 0))
+                    and (ec.position_id is null  or (ec.position_id is not null and ep.delete = 0))
+            order by u.id asc
+            ) as euce
+            
+            group by euce.id
+            order by euce.id asc
+            limit " . $limit_start . "," . $limit_end . " ;";
         //echo $sql;die;
         return $base->getFetchAll($sql);
     }
