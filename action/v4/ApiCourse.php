28a05204733395251c622a88f7d62da26dc64d8f
spacehu <spacehu@tigerhuclub.com>
2019-11-05 20:53:25 +0800

    update bi

--- action/v4/ApiCourse.php
+++ action/v4/ApiCourse.php
@@ -150,10 +150,10 @@ class ApiCourse extends \action\RestfulApi {
             return self::$data;
         }
         try {
-            $enterprise_id = AccountDAL::getEnterpriseUser($this->user_id)['enterprise_id'];
+            //$enterprise_id = AccountDAL::getEnterpriseUser($this->user_id)['enterprise_id'];
             //轮播列表
             $_obj = CourseDAL::getOne($this->get['course_id'], $this->user_id);
-            $res = TestDAL::getRand($this->get['course_id'], $_obj['text_max'],$enterprise_id);
+            $res = TestDAL::getRand($this->get['course_id'], $_obj['text_max']);
             //print_r($res);die;
             self::$data['data']['list'] = $res;
             self::$data['data']['total'] = count($res);
